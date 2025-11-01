bdb46ec20aec7e0fdffa0d4f47893699d9ba244c
Sridharon5 <malothsridharvarma@gmail.com>
2025-11-01 20:30:20 +0530

    updated

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:false,
-    userUrl: 'http://flavor-haven-3.onrender.com/api/',
+    userUrl: 'https://flavor-haven-3.onrender.com/api/',
    spoonacularUrl:'https://api.spoonacular.com/recipes/'
 };
