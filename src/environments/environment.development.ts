6ebf80812aece6b120e279cff796b70ec9b7e951
Sridharon5 <malothsridharvarma@gmail.com>
2025-11-01 21:37:41 +0530

    Update environment.developement.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  userUrl: 'https://flavor-haven-3.onrender.com/api/',
+  userUrl: 'https://flavor-haven-4.onrender.com/api/',
   spoonacularUrl: 'http://flavor-haven-4.onrender.com/api/spoonacular/',
 };
